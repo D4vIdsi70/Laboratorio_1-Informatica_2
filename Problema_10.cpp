81f88dbfdea0396f23d0a61b2fad3253bd0ae8e2
D4vIdsi70 <142255916+D4vIdsi70@users.noreply.github.com>
2023-10-02 09:59:57 -0500

    pero aún no arreglo lo de la resta qwq

---
+++ Problema_10.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string.h>
+#include <ctype.h>
+
+using namespace std;
+
+int main()
+{
+    char cadena[255];
+    int longitud;
+    cout << "Ingrese la cadena de caracteres: ";
+    cin.getline(cadena, 255, '\n' );
+    longitud = strlen(cadena);
+    int *resultado = new int[longitud];
+    for(int i = 0; i<longitud; i++){
+        if(cadena[i] == 77){
+            resultado[i] = 1000;
+        }else if(cadena[i] == 68){
+            resultado[i] = 500;
+        }else if(cadena[i] == 67){
+            resultado[i] = 100;
+        }else if(cadena[i] == 76){
+            resultado[i] = 50;
+        }else if(cadena[i] == 88){
+            resultado[i] = 10;
+        }else if(cadena[i] == 86){
+            resultado[i] = 5;
+        }else if(cadena[i] == 73){
+            resultado[i] = 1;
+        }
+    }
+    int total = resultado[0];
+    
+    for(int i = 1; i < longitud; i++){
+        if(resultado[i] > resultado[i - 1]){
+            total -= resultado[i];
+        }
+        else{
+            total += resultado[i];
+        } 
+    }
+    cout << "El número ingresado es " << cadena << endl;
+    cout << "Que corresponde a " << total;
+    delete []resultado;
+    return 0;
+}
